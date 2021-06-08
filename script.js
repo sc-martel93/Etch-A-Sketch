df0a454dfeaf1183e20546287d0368dc2425b2d9
Scott Martel <sc.martel93@gmail.com>
2021-06-08 11:35:23 -0400

    Create grid function implementation

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ let container = document.querySelector('.container')
 const INITIAL_GRID_SIZE = 16
 
 const createGrid = (gridSize) => {
-    
-    for(let i = 0; i < gridSize; i++){
-        let gridElement = document.createElement('div')
-        
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr )`
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr )`
+
+    for(let j = 0; j < gridSize; j++){
+            let gridElement = document.createElement('div')
+            gridElement.classList = "grid-element"
+            container.appendChild(gridElement)
+            // add hover event
+            
+        for(let i = 0; i < gridSize; i++){
+            let gridElement = document.createElement('div')
+            gridElement.classList = "grid-element"
+            container.appendChild(gridElement)
+            // add hover event
+        }
     }
+    
 }
+
+createGrid(INITIAL_GRID_SIZE)
