7ebf9da5174f9af62386057af383363fe60b8251
Scott Martel <sc.martel93@gmail.com>
2021-06-08 11:23:18 -0400

    Set up container

---
+++ script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector('.container')
+const INITIAL_GRID_SIZE = 16
+
+const createGrid = (gridSize) => {
+    
+    for(let i = 0; i < gridSize; i++){
+        let gridElement = document.createElement('div')
+        
+    }
+}
