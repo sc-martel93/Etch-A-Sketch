5575bd8a2e396fdadf24d598a29a03e6c8d7a552
Scott Martel <sc.martel93@gmail.com>
2021-06-08 11:51:27 -0400

    Refactor grid creation function for simplicity

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ let container = document.querySelector('.container')
 const INITIAL_GRID_SIZE = 16
 
 const createGrid = (gridSize) => {
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr )`
-
-    for(let j = 0; j < gridSize; j++){
-            let gridElement = document.createElement('div')
-            gridElement.classList = "grid-element"
-            container.appendChild(gridElement)
-            // add hover event
-            
-        for(let i = 0; i < gridSize; i++){
-            let gridElement = document.createElement('div')
-            gridElement.classList = "grid-element"
-            container.appendChild(gridElement)
-            // add hover event
-        }
+    let gridArea = gridSize * gridSize
+   
+    for(let i = 0; i < gridArea; i++){
+        let gridElement = document.createElement('div')
+        gridElement.classList = "grid-element"
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr )`
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr )`
+        container.insertAdjacentElement('beforeend', gridElement)
     }
+
     
 }
 
 createGrid(INITIAL_GRID_SIZE)
+
+const changeColor = () => {
+    this.backgroundColor = 'black'
+}
